4810c3458a2f2e2897fb8fbf966b82d6f8a7ec23
Geoff Matrangola <geoff@matrangola.com>
2018-03-05 17:49:36 -0500

    Added capability to set the Min and Max Advertisement Interval. Added example that must be run as root.

--- src/main/java/it/tangodev/ble/BleApplication.java
+++ src/main/java/it/tangodev/ble/BleApplication.java
@@ -17,6 +17,7 @@ import org.freedesktop.dbus.exceptions.DBusException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,7 +30,7 @@ import java.util.Map;
  *
  */
 public class BleApplication implements GattApplication1 {
-    private static final Logger LOG = LoggerFactory.getLogger(BleApplication.class);
+	private static final Logger LOG = LoggerFactory.getLogger(BleApplication.class);
 
 	public static final String DBUS_BUSNAME = "org.freedesktop.DBus";
 	public static final String BLUEZ_DBUS_BUSNAME = "org.bluez";
@@ -38,6 +39,11 @@ public class BleApplication implements GattApplication1 {
 	public static final String BLUEZ_GATT_INTERFACE = "org.bluez.GattManager1";
 	public static final String BLUEZ_LE_ADV_INTERFACE = "org.bluez.LEAdvertisingManager1";
 	public static final String ADDRESS = "Address";
+	public static final String KERNEL_DEBUG_PATH = "/sys/kernel/debug/bluetooth/";
+	public static final String ADV_MIN_INTERVAL_FILENAME = "/adv_min_interval";
+	public static final String ADV_MAX_INTERVAL_FILENAME = "/adv_max_interval";
+	public static final int MIN_ADVERTISE_INtERVAL = 20; // in ms
+	public static final int MAX_ADVERTISE_INTERVAL = 20240; // in ms, 20.24 s
 
 	private List<BleService> servicesList = new ArrayList<BleService>();
 	private String path = null;
@@ -68,7 +74,71 @@ public class BleApplication implements GattApplication1 {
 		this.path = path;
 		this.listener = listener;
 	}
-	
+
+	/**
+	 * Sets the minimum advertise interval for the Peripherals Service. Same value is used for all services on this
+	 * device. Must be lower than the MAX interval. Requires Root.
+	 * @param adapter usually hci0
+	 * @param min int in milliseconds value from 20ms - (max - 1ms)
+	 * @throws IOException if it cannot read or write the file
+	 * @throws InvalidSettingException if min is not a valid range
+	 */
+	public static void setBleAdvertiseIntervalMin(String adapter, int min) throws IOException, InvalidSettingException {
+		if (min < MIN_ADVERTISE_INtERVAL) throw new InvalidSettingException(
+				"IntervalMin must be between 20 and (max - 1)");
+		int max = getBleAdvertiseIntervalMax(adapter);
+		if (min >= max) throw new InvalidSettingException("Min Interval must be less than max");
+		setKernelSetting(adapter, min, ADV_MIN_INTERVAL_FILENAME);
+	}
+
+	/**
+	 * Sets the maximum advertise interval for the Peripherals' Services. This is the max delay between each
+	 * advertisement + 0-10ms random. Lower number means more frequent advertisements and easier for the Centrals to
+	 * see, but more power consumption. Same value is used for all services on this device. Must be greater than the MIN
+	 * interval. Requires. Root.
+	 * @param adapter usually hci0
+	 * @param max int in milliseconds value from min - 20.24s
+	 * @throws IOException if unable to access the kernel file
+	 * @throws InvalidSettingException if max is not a valid range
+	 */
+	public static void setBleAdvertiseIntervalMax(String adapter, int max) throws IOException, InvalidSettingException {
+		int min = getBleAdvertiseIntervalMin(adapter);
+		if (max > MAX_ADVERTISE_INTERVAL) throw new InvalidSettingException(
+				"MintervalMax must be between min and 10.24 seconds");
+		if (max <= min) throw new InvalidSettingException("Max Interval must be larger than min.");
+		setKernelSetting(adapter, max, ADV_MAX_INTERVAL_FILENAME);
+	}
+
+	public static int getBleAdvertiseIntervalMin(String adapter) throws IOException {
+		return getKernelSetting(adapter, ADV_MIN_INTERVAL_FILENAME);
+	}
+
+	public static int getBleAdvertiseIntervalMax(String adapter) throws IOException {
+		return getKernelSetting(adapter, ADV_MAX_INTERVAL_FILENAME);
+	}
+
+	private static int getKernelSetting(String adapter, String filename) throws IOException {
+		String path = KERNEL_DEBUG_PATH + adapter + filename;
+		File file = new File(path);
+		if (!file.exists()) throw new FileNotFoundException("No file " + path);
+		int result;
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			String line = reader.readLine();
+			result = Integer.parseInt(line);
+		}
+		return result;
+	}
+
+	private static void setKernelSetting(String adapter, int min, String filename) throws IOException {
+		String path = KERNEL_DEBUG_PATH + adapter + filename;
+		File file = new File(path);
+		if (!file.exists()) throw new FileNotFoundException("No file " + path);
+		try (FileOutputStream fos = new FileOutputStream(file)) {
+			fos.write(Integer.toString(min).getBytes());
+		}
+	}
+
+
 	/**
 	 * First of all the method power-on the adapter.
 	 * Then publish the service with their characteristic and start the advertisement (only primary service can advertise).
